126cdaaeb8ba8f3e1ed34543c0ba49527027cc7f
Dobrosław Żybort <matrixik@gmail.com>
2017-03-09 11:04:37 +0100

    Update monasca-agent cookbook and barclamp (#2)
    * Add monasca-reconfigure and sudoers file
    
    * Fix monasca-setup bin path
    
    * Fix template variables
    
    * Keystone settings for agent
    
    * Keystone settings for agent
    
    * Keystone settings for agent
    
    * Add log
    
    * More readable config templates
    
    * Remove log
    
    * Remove per-node credentials todos
    
    * Fix json indentations
    
    * Rename agent dimensions variable name
    
    * Run monasca-reconfigure every time with chef client
    
    * Add firts GUI elements
    
    * Add GUI elements
    
    * Update for new code arrangement for keystone
    
    * Fix yaml for translations
    
    * Fix monasca helper module name
    
    * Use root user for agent temporally
    
    * Update monasca agent GUI parts
    
    * WIP: Add custom checks for monasca-agent
    
    * Extract variables for templates
    
    * Fix testing filepath
    
    * Fix template for monasca agent checks
    
    * Fix template paths again
    
    * Address Hound warning
    
    * Add more monasca agent checkers
    
    * Fix some variables
    
    * Add log api port to data_bag
    
    * Build Kibana and log-api urls
    
    * Add more info to GUI
    
    * Add user_managed: false
    
    * Fix yaml indentation
    
    * Add some logs
    
    * Fix monasca-reconfigure
    
    * Fix ruby boolean name
    
    * Uncomment agent.yaml template code
    
    * Fix providing service variable to template
    
    * Fix insecure args for monasca-setup
    
    * Fix monasca-reconfigure
    
    * Configure more variables with monasca-reconfigure
    
    * Add more config to UI
    
    * Disable generating agent.yaml
    
    It's regenerated every time chef client is run, it's running
    monasca-reconfigure.
    
    * Change default overwrite config to false
    
    * Add monasca-agent to sudoers
    
    * Run monasca-setup as monasca-agent user
    
    * Remove logs from monasca-reconfigure
    
    * Rename monasca-agent to monasca-metric-agent
    
    And rename all variables to reflect this change.
    
    * Fix hound warnings
    
    * Address comments
    
    * Fix copyright
    
    * Use same order in data_bag schema like in json
    
    * Restore user mapping in log_agent schema
    
    * Change monasca-master unique to false
    
    * Fix monasca-metric-agents names in json
    
    * Use keystone settings from data_bag for metric agent
    
    * Replace `owner` with `user` in execute command
    
    * Replace group `monasca-agent` with `monasca`
    
    * Remove adding monasca-agent to sudoers
    
    Will be added from rpm.
    
    * Add port to monasca_api_url for monasca-agent
    
    * Use helper for getting api url
    
    * Fix bad default config name

--- chef/cookbooks/monasca/libraries/helper.rb
+++ chef/cookbooks/monasca/libraries/helper.rb
@@ -25,8 +25,8 @@ module MonascaHelper
     CrowbarHelper.get_host_for_admin_url(node, ha_enabled)
   end
 
-  def self.api_admin_url(node)
-    host = api_admin_host(node)
+  def self.api_public_url(node)
+    host = monasca_public_host(node)
     protocol = node[:monasca][:api][:ssl] ? "https" : "http"
     port = node[:monasca][:api][:bind_port]
     "#{protocol}://#{host}:#{port}/v2.0"
